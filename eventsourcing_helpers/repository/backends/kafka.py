62582b74b0c0cdc5294b516f6383acdfc0e6555d
Simon Wahlgren <simon.wahlgren@fyndiq.se>
2017-08-23 11:41:52 +0200

    Fetch schemas from schema registry

--- eventsourcing_helpers/repository/backends/kafka.py
+++ eventsourcing_helpers/repository/backends/kafka.py
@@ -1,4 +1,6 @@
 from confluent_kafka.avro import AvroProducer
+from confluent_kafka.avro.cached_schema_registry_client import (
+    CachedSchemaRegistryClient)
 
 from confluent_kafka_helpers.loader import AvroMessageLoader
 
@@ -8,22 +10,30 @@ from eventsourcing_helpers.message import to_message_from_dto
 
 class KafkaBackend:
 
-    def __init__(self, topic, producer_config, loader_config,
-                 default_value_schema, default_key_schema):
+    def __init__(self, topic, producer_config, loader_config):
         self.topic = topic
         self.producer_config = producer_config
-        self.default_value_schema = default_value_schema
-        self.default_key_schema = default_key_schema
+
+        schema_registry_url = producer_config['schema.registry.url']
+        key_subject_name = producer_config.pop('key_subject_name')
+        value_subject_name = producer_config.pop('value_subject_name')
+        schema_registry = CachedSchemaRegistryClient(
+            url=schema_registry_url
+        )
+        # fetch latest schemas from schema registry
+        key_schema = schema_registry.get_latest_schema(key_subject_name)
+        value_schema = schema_registry.get_latest_schema(value_subject_name)
 
         self.producer = AvroProducer(
             producer_config,
-            default_value_schema=default_value_schema,
-            default_key_schema=default_key_schema
+            default_key_schema=key_schema[1],
+            default_value_schema=value_schema[1]
         )
-        self.loader = AvroMessageLoader(loader_config)
+        self.loader = AvroMessageLoader(loader_config, schema_registry)
 
     def _publish(self, topic, key, value):
-        logger.info("Publishing message", topic=topic, key=key, value=value)
+        logger.info("Publishing message", topic=topic, key=key,
+                    value=value)
         self.producer.produce(topic=topic, key=key, value=value)
         self.producer.flush()
 
