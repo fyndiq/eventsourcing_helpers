0681732c406985d2783d21c0aeac815aefb1c5f9
Simon Wahlgren <simon.wahlgren@fyndiq.se>
2017-08-24 09:27:34 +0200

    Fetch schemas in AvroLoader

--- eventsourcing_helpers/repository/backends/kafka.py
+++ eventsourcing_helpers/repository/backends/kafka.py
@@ -1,6 +1,5 @@
 from confluent_kafka_helpers.loader import AvroMessageLoader
 from confluent_kafka_helpers.producer import AvroProducer
-from confluent_kafka_helpers.schema_registry import SchemaRegistry
 
 from eventsourcing_helpers import logger
 from eventsourcing_helpers.message import to_message_from_dto
@@ -10,32 +9,17 @@ class KafkaBackend:
 
     def __init__(self, topic, producer_config, loader_config):
         self.topic = topic
-        self.producer_config = producer_config
-
-        schema_registry_url = producer_config['schema.registry.url']
-        key_subject_name = producer_config.pop('key_subject_name')
-        value_subject_name = producer_config.pop('value_subject_name')
-
-        # fetch latest schemas from schema registry
-        schema_registry = SchemaRegistry(schema_registry_url)
-        key_schema = schema_registry.get_latest_schema(key_subject_name)
-        value_schema = schema_registry.get_latest_schema(value_subject_name)
-
-        self.producer = AvroProducer(
-            producer_config,
-            key_schema=key_schema,
-            value_schema=value_schema
-        )
+        self.producer = AvroProducer(producer_config)
         self.loader = AvroMessageLoader(loader_config)
 
-    def save(self, aggregate, topic=None):
+    def save(self, aggregate, topic=None, **kwargs):
         if not topic:
             topic = self.topic
 
         for event in aggregate._uncommitted_events:
             message = to_message_from_dto(event)
             self.producer.publish(topic=topic, key=aggregate._id,
-                                  value=message)
+                                  value=message, **kwargs)
 
         aggregate.mark_events_as_commited()
 
