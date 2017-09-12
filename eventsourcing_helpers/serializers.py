a198e7f4532c57c55cb97bc39a3f2eed514e692d
Lucas Magnum <lucasmagnum2@gmail.com>
2017-09-12 15:26:53 +0200

    Update serializers.py

--- eventsourcing_helpers/serializers.py
+++ eventsourcing_helpers/serializers.py
@@ -3,7 +3,17 @@ from collections import namedtuple
 
 def from_message_to_dto(message):
     """
-    Serialize a message to a DTO.
+    Serialize a message to a Data Transfer Object (DTO).
+
+        >> message = {
+            'class': 'OrderCompleted',
+            'data': {
+                'order_id': 'UA123',
+                'date': '2017-09-08'
+            }
+        }
+        >> from_message_to_dto(message)
+        OrderCompleted(order_id='UA123', date='2017-09-08')
     """
     obj = namedtuple(message['class'], message['data'].keys())
     dto = obj(**message['data'])
@@ -13,7 +23,17 @@ def from_message_to_dto(message):
 
 def to_message_from_dto(dto):
     """
-    Serialize a DTO to a message.
+    Serialize a Data Transfer Object (DTO) to a message.
+
+        >> dto = OrderCompleted(order_id='UA123', date='2017-09-08')
+        >> to_message_from_dto(dto)
+        >> {
+            'class': 'OrderCompleted',
+            'data': {
+                'order_id': 'UA123',
+                'date': '2017-09-08'
+            }
+        }
     """
     message = {
         'class': dto.__class__.__name__,
