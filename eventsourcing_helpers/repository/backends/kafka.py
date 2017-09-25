f882e69a994e65028ba615b6c080455776902464
Simon Wahlgren <simon.wahlgren@fyndiq.se>
2017-09-25 11:34:15 +0200

    Add docstring and type hints

--- eventsourcing_helpers/repository/backends/kafka.py
+++ eventsourcing_helpers/repository/backends/kafka.py
@@ -6,13 +6,30 @@ from eventsourcing_helpers.repository.backends.base import RepositoryBackend
 
 class KafkaBackend(RepositoryBackend):
 
-    def __init__(self, config, producer=AvroProducer, loader=AvroMessageLoader):
+    def __init__(self, config: dict, producer=AvroProducer,
+                 loader=AvroMessageLoader) -> None:
         self.producer = producer(config.get('producer'))
         self.loader = loader(config.get('loader'))
 
-    def commit(self, guid, events, **kwargs):
+    def commit(self, guid, events, **kwargs) -> None:
+        """
+        Commit events to Kafka.
+
+        Args:
+            guid: Aggregate root guid to be used as key in the message.
+            events: List of staged events to be committed.
+        """
         for event in events:
             self.producer.produce(key=guid, value=event, **kwargs)
 
-    def load(self, guid, *args, **kwargs):
-        return self.loader.load(guid, *args, **kwargs)
+    def load(self, guid, **kwargs) -> list:
+        """
+        Load events from Kafka.
+
+        Args:
+            guid: Aggregate root guid to load.
+
+        Returns:
+            list: Loaded events.
+        """
+        return self.loader.load(guid, **kwargs)
