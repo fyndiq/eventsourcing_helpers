d1dd8eecd77b63682caf5f8e26443294ec79add7
Simon Wahlgren <simon.wahlgren@fyndiq.se>
2017-09-26 15:27:49 +0200

    Add docstrings and type hints

--- eventsourcing_helpers/message.py
+++ eventsourcing_helpers/message.py
@@ -1,36 +1,51 @@
-from typing import NamedTuple
+from typing import Callable, NamedTuple
 
 
 class Message:
 
-    def __init__(self, cls, *args, **kwargs):
-        assert not isinstance(cls, NamedTuple)
-        self.__dict__['message'] = cls(**kwargs)
+    def __init__(self, cls: NamedTuple, **kwargs) -> None:
+        self.__dict__['message'] = cls(**kwargs)  # type: ignore
 
     @property
-    def _name(self):
+    def _name(self) -> str:
         return self.message.__class__.__name__
 
-    def to_dict(self):
-        return self.message._asdict()
+    def to_dict(self) -> dict:
+        return dict(self.message._asdict())  # type: ignore
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return repr(self.message)
 
-    def __getattr__(self, name):
+    def __getattr__(self, name: str) -> Callable:
         return getattr(self.message, name)
 
-    def __setattr__(self, *args):
+    def __setattr__(self, *args) -> None:
         raise AttributeError("Messages are read only")
 
 
-def message_factory(cls):
-    class WrappedMessage(Message):
-        def __init__(self, *args, **kwargs):
-            super().__init__(cls, *args, **kwargs)
+def message_factory(cls: NamedTuple) -> type:
+    """
+    Class decorator used for "enriching" a namedtuple object with some common
+    features.
 
-    wrapped = type(cls.__name__, (WrappedMessage, object), {})
-    return wrapped
+    Currently namedtuples doesn't support inheritance from a base class - which
+    means we can't easily add common methods/properties for a message.
+
+    See: https://github.com/python/typing/issues/427.
+
+    To work around this we can wrap the namedtuple in a "proxy class" which
+    adds all the common features and redirects all attribute lookups to the
+    underlying namedtuple.
+    """
+
+    class MessageProxy(Message):
+
+        def __init__(self, *args, **kwargs) -> None:
+            super().__init__(cls, **kwargs)
+
+    # create a new proxy class that inherits from MessageProxy
+    proxy = type(cls.__name__, (MessageProxy, object), {})  # type: ignore
+    return proxy
 
 
 event = lambda cls: message_factory(cls)
