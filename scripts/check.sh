73a81648d9277f521b2cb3db84e17fbf871742e2
Simon Wahlgren <simon.wahlgren@fyndiq.se>
2017-09-29 11:10:40 +0200

    Enable virtual env

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env sh
+[[ -z "${VIRTUAL_ENV}" ]] && . .venv/bin/activate
 mypy eventsourcing_helpers/ --ignore-missing-imports --show-error-context
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env sh
+[[ -z "${VIRTUAL_ENV}" ]] && . .venv/bin/activate
 pytest --cov=eventsourcing_helpers/
