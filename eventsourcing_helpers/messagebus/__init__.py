09142daf14dc58348669b561e752a2a7bc0a1b45
Simon Wahlgren <simon.wahlgren@fyndiq.se>
2017-10-02 13:13:12 +0200

    Add comments

--- eventsourcing_helpers/messagebus/__init__.py
+++ eventsourcing_helpers/messagebus/__init__.py
@@ -10,13 +10,18 @@ BACKENDS_PACKAGE = 'eventsourcing_helpers.messagebus.backends'
 
 
 class MessageBus:
+    """
+    Generic interface to communicate with a message bus backend.
 
+    The interface provides methods for producing and consuming
+    messages (commands, events).
+    """
     DEFAULT_BACKEND = 'kafka_avro'
 
     def __init__(self, config: dict, importer: Callable=import_backend) -> None:
-        backend = config.pop('backend', self.DEFAULT_BACKEND)
+        backend = config.get('backend', self.DEFAULT_BACKEND)
         assert 'backend_config' in config, "You must pass a backend config"
-        backend_config = config.pop('backend_config')
+        backend_config = config.get('backend_config')
 
         logger.debug("Using message bus backend", backend=backend,
                      config=backend_config)
@@ -24,7 +29,20 @@ class MessageBus:
         self.backend = backend_class(backend_config)
 
     def produce(self, key, value, **kwargs):
+        """
+        Produce a message to the message bus.
+
+        Args:
+            key: Key to produce message with.
+            value: Value to produce message with.
+        """
         self.backend.produce(key, value, **kwargs)
 
     def get_consumer(self):
+        """
+        Get a message consumer.
+
+        Returns:
+            Callable: Message consumer.
+        """
         return self.backend.get_consumer()
