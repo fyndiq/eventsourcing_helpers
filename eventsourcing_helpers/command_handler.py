a2b3614e54d632fcaa5a7a6eb56378fa8dccc781
Simon Wahlgren <simon.wahlgren@fyndiq.se>
2017-10-18 14:21:06 +0200

    New command handler class that's not event sourced

--- eventsourcing_helpers/command_handler.py
+++ eventsourcing_helpers/command_handler.py
@@ -11,22 +11,17 @@ logger = structlog.get_logger(__name__)
 
 class CommandHandler:
     """
-    A domain service that turns a command into events by calling methods on an
-    aggregate root, publishes the resulting events, and handles transactions
-    and persistence.
+    Command handler.
+
+    Handles a command by calling the correct function or method
+    in a handler class.
     """
-    aggregate_root: AggregateRoot = None
     handlers: dict = {}
-    repository_config: dict = None
 
-    def __init__(self, message_deserializer: Callable=from_message_to_dto,
-                 repository: Any=Repository) -> None:
-        assert self.aggregate_root
+    def __init__(self, message_deserializer: Callable=
+                 from_message_to_dto) -> None:  # yapf: disable
         assert self.handlers
-        assert self.repository_config
-
         self.message_deserializer = message_deserializer
-        self.repository = repository(self.repository_config)
 
     def _can_handle_command(self, message: dict) -> bool:
         """
@@ -44,12 +39,64 @@ class CommandHandler:
 
         return True
 
+    def _handle_command(self, command: Any, handler_class: Any = None) -> None:
+        """
+        Get and call the correct command handler.
+
+        The handler can either be a callable or a name of a method
+        in the handler class.
+
+        Args:
+            command: Command to be handled.
+            handler_class: Optional class with handler methods.
+        """
+        command_class = command._class
+        handler = self.handlers[command_class]
+        logger.info("Calling command handler", command_class=command_class)
+        if not callable(handler):
+            handler = getattr(handler_class, handler)
+        handler(command)
+
+    def handle(self, message: dict) -> None:
+        """
+        Apply correct handler for the received command.
+
+        Args:
+            message: Consumed message from the bus.
+        """
+        if not self._can_handle_command(message):
+            return
+
+        command = self.message_deserializer(message)
+        logger.info("Handling command", command_class=command._class)
+        self._handle_command(command)
+
+
+class ESCommandHandler(CommandHandler):
+    """
+    Event sourced command handler.
+
+    Unlike the command handler we expect an aggregate root to handle all
+    the commands. The resulting staged events are published to a message
+    bus and persisted in an event store using a repository.
+    """
+    aggregate_root: AggregateRoot = None
+    repository_config: dict = None
+
+    def __init__(self, message_deserializer: Callable=from_message_to_dto,
+                 repository: Any=Repository) -> None:  # yapf: disable
+        super().__init__(message_deserializer)
+        assert self.aggregate_root
+        assert self.repository_config
+
+        self.repository = repository(self.repository_config)
+
     def _get_events(self, guid: str) -> List[Any]:
         """
         Get all events for an aggregate root from the repository.
 
         Args:
-            guid: Get all events with this guid.
+            guid: Get all events for this guid.
 
         Returns:
             list: List with all events.
@@ -76,24 +123,6 @@ class CommandHandler:
 
         return aggregate_root
 
-    def _handle_command(self, aggregate_root: AggregateRoot,
-                        command: Any) -> None:
-        """
-        Get and call the correct command handler.
-
-        Args:
-            aggregate_root: Entity which implements the command handlers.
-            command: Command to be handled.
-        """
-        command_class = command._class
-        handler_name = self.handlers[command_class]
-        logger.info("Calling command handler", command_class=command_class,
-                    handler_name=handler_name,
-                    aggregate_root=aggregate_root._class)
-
-        handler = getattr(aggregate_root, handler_name)
-        handler(command)
-
     def _commit_staged_events(self, aggregate_root: AggregateRoot) -> None:
         """
         Commit staged events to the repository.
@@ -105,7 +134,7 @@ class CommandHandler:
 
     def handle(self, message: dict) -> None:
         """
-        Apply correct handler for received command.
+        Apply correct handler for the received command.
 
         After the command has been handled the staged
         events are committed to the repository.
@@ -120,5 +149,5 @@ class CommandHandler:
         logger.info("Handling command", command_class=command._class)
 
         aggregate_root = self._get_aggregate_root(command.guid)
-        self._handle_command(aggregate_root, command)
+        self._handle_command(command, handler_class=aggregate_root)
         self._commit_staged_events(aggregate_root)
--- tests/test_command_handler.py
+++ tests/test_command_handler.py
@@ -1,58 +1,63 @@
+from copy import deepcopy
 from unittest.mock import Mock, patch
 
-from eventsourcing_helpers.command_handler import CommandHandler
+from eventsourcing_helpers.command_handler import (
+    CommandHandler, ESCommandHandler)
 
 module = 'eventsourcing_helpers.command_handler'
 
+command_class, guid = 'FooCommand', '1'
+message = {'class': command_class, 'data': {'guid': guid}}
+events = [1, 2, 3]
 
-class CommandHandlerTests:
+command = Mock()
+command._class = command_class
+command.guid = guid
+
+message_deserializer = Mock()
+message_deserializer.return_value = command
+
+
+class ESCommandHandlerTests:
+
+    def teardown_method(self):
+        message_deserializer.reset_mock()
 
     def setup_method(self):
-        command_class, guid = 'FooCommand', '1'
-
-        self.message = {'class': command_class, 'data': {'guid': guid}}
-        self.events = [1, 2, 3]
         self.aggregate_root = Mock()
 
-        self.command = Mock()
-        self.command._class = command_class
-        self.command.guid = guid
-
-        config = {'return_value.load.return_value': self.events}
+        config = {'return_value.load.return_value': events}
         self.repository = Mock()
         self.repository.configure_mock(**config)
 
-        self.message_deserializer = Mock()
-        self.message_deserializer.return_value = self.command
+        command_handler = ESCommandHandler
+        command_handler.aggregate_root = self.aggregate_root
+        command_handler.handlers = {command_class: 'foo_method'}
+        command_handler.repository_config = {'empty_config': None}
 
-        handler = CommandHandler
-        handler.aggregate_root = self.aggregate_root
-        handler.handlers = {command_class: 'foo_method'}
-        handler.repository_config = {'empty_config': None}
-
-        self.handler = handler(
-            message_deserializer=self.message_deserializer,
+        self.handler = command_handler(
+            message_deserializer=message_deserializer,
             repository=self.repository
         )
 
-    @patch(f'{module}.CommandHandler._commit_staged_events')
-    @patch(f'{module}.CommandHandler._handle_command')
-    @patch(f'{module}.CommandHandler._get_aggregate_root')
-    @patch(f'{module}.CommandHandler._can_handle_command')
+    @patch(f'{module}.ESCommandHandler._commit_staged_events')
+    @patch(f'{module}.ESCommandHandler._handle_command')
+    @patch(f'{module}.ESCommandHandler._get_aggregate_root')
+    @patch(f'{module}.ESCommandHandler._can_handle_command')
     def test_handle(self, mock_can_handle, mock_get, mock_handle, mock_commit):
         """
-        Test that correct methods are invoked when handling a command.
+        Test that the correct methods are invoked when handling a command.
         """
         mock_can_handle.return_value = True
         mock_get.return_value = self.aggregate_root
 
-        self.handler.handle(self.message)
+        self.handler.handle(message)
 
-        self.message_deserializer.assert_called_once_with(self.message)
-        mock_can_handle.assert_called_once_with(self.message)
-        mock_get.assert_called_once_with(self.command.guid)
+        message_deserializer.assert_called_once_with(message)
+        mock_can_handle.assert_called_once_with(message)
+        mock_get.assert_called_once_with(command.guid)
         mock_handle.assert_called_once_with(
-            self.aggregate_root, self.command
+            command, handler_class=self.aggregate_root
         )
         mock_commit.assert_called_once_with(self.aggregate_root)
 
@@ -69,45 +74,81 @@ class CommandHandlerTests:
         """
         Test that the correct command handler is invoked.
         """
-        self.handler._handle_command(
-            self.aggregate_root, self.command
-        )
-        self.aggregate_root.foo_method.called_once_with(self.command)
+        self.handler._handle_command(command, handler_class=self.aggregate_root)
+        self.aggregate_root.foo_method.called_once_with(command)
 
-    @patch(f'{module}.CommandHandler._get_events')
+    @patch(f'{module}.ESCommandHandler._get_events')
     def test_get_aggregate_root(self, mock_get_events):
         """
         Test that we get the correct aggregate root and that the correct
         methods are invoked.
         """
-        mock_get_events.return_value = self.events
-        aggregate_root = self.handler._get_aggregate_root(self.command.guid)
+        mock_get_events.return_value = events
+        aggregate_root = self.handler._get_aggregate_root(command.guid)
 
         self.aggregate_root.return_value == aggregate_root
-        mock_get_events.assert_called_once_with(self.command.guid)
-        self.aggregate_root.apply_events.called_once_with(self.events)
+        mock_get_events.assert_called_once_with(command.guid)
+        self.aggregate_root.apply_events.called_once_with(events)
 
     def test_get_events(self):
         """
         Test that we get the correct events and that the correct methods
         are invoked.
         """
-        self.message_deserializer.side_effect = lambda m: m
-        events = self.handler._get_events(self.command.guid)
+        message_deserializer.side_effect = lambda m: m
+        _events = self.handler._get_events(command.guid)
 
-        assert self.events == events
-        self.repository.return_value.load.assert_called_once_with(
-            self.command.guid
+        assert events == _events
+        self.repository.return_value.load.assert_called_once_with(command.guid)
+        assert message_deserializer.call_count == len(events)
+
+        message_deserializer.side_effect = None
+
+
+class CommandHandlerTests:
+
+    def teardown_method(self):
+        message_deserializer.reset_mock()
+
+    def setup_method(self):
+        self.mock_handler = Mock()
+
+        command_handler = CommandHandler
+        command_handler.handlers = {command_class: self.mock_handler}
+
+        self.handler = command_handler(
+            message_deserializer=message_deserializer
         )
-        assert self.message_deserializer.call_count == len(self.events)
+
+    @patch(f'{module}.CommandHandler._handle_command')
+    @patch(f'{module}.CommandHandler._can_handle_command')
+    def test_handle(self, mock_can_handle, mock_handle):
+        """
+        Test that the correct methods are invoked when handling a command.
+        """
+        mock_can_handle.return_value = True
+
+        self.handler.handle(message)
+
+        message_deserializer.assert_called_once_with(message)
+        mock_can_handle.assert_called_once_with(message)
+        mock_handle.assert_called_once_with(command)
 
     def test_can_handle_command(self):
         """
         Test that we only handle registered commands.
         """
-        can_handle = self.handler._can_handle_command(self.message)
+        can_handle = self.handler._can_handle_command(message)
         assert can_handle is True
 
-        self.message['class'] = 'BarCommand'
-        can_handle = self.handler._can_handle_command(self.message)
+        _message = deepcopy(message)
+        _message['class'] = 'BarCommand'
+        can_handle = self.handler._can_handle_command(_message)
         assert can_handle is False
+
+    def test_handle_command(self):
+        """
+        Test that the correct command handler is invoked.
+        """
+        self.handler._handle_command(command)
+        self.mock_handler.called_once_with(command)
