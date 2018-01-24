1d5a9fc52cd4095ef33137c3ab9079488d6fb7d5
Simon Wahlgren <simon.wahlgren@fyndiq.se>
2018-01-24 16:08:06 +0100

    Exit on failures

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,9 @@
-#!/usr/bin/env sh
+#!/bin/bash
+set -e
+
 [[ -z "${VIRTUAL_ENV}" ]] && . .venv/bin/activate
 pytest -s --cov=eventsourcing_helpers/ --junitxml=/tmp/test-results/report.xml
 
 if [ "$1" == "ci" ]; then
-    codecov -t $CODECOV_TOKEN
+    codecov
 fi
