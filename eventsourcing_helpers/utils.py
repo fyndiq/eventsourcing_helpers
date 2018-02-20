1dadecfcbb60b19886a67c4b95b256f4a37c3759
Simon Wahlgren <simon.wahlgren@fyndiq.se>
2018-02-20 10:24:29 +0100

    Fix flake8 warnings

--- eventsourcing_helpers/utils.py
+++ eventsourcing_helpers/utils.py
@@ -17,7 +17,7 @@ def import_backend(location: str) -> Any:
         >>> import_backend(
             'eventsourcing_helpers.repository.backends.kafka.KafkaAvroBackend'
         )
-        <class 'eventsourcing_helpers.repository.backends.kafka.KafkaAvroBackend'>
+        <class 'eventsourcing_helpers.repository.backends.kafka.KafkaAvroBackend'>  # noqa
     """
     module_name, class_name = location.rsplit('.', 1)
     module = import_module(f'{module_name}')
--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 from setuptools import setup, find_packages
 
-setup(name="eventsourcing-helpers",
-      version="0.6.2",
-      description="Helpers for practicing the Event sourcing pattern",
-      url="https://github.com/fyndiq/eventsourcing_helpers",
-      author="Fyndiq AB",
-      author_email="support@fyndiq.com",
-      license="MIT",
-      packages=find_packages(),
-      install_requires=[
-          'structlog>=17.2.0',
-          'cnamedtuple>=0.1.6',
-          'confluent-kafka-helpers==0.5.2',
-      ],
-      dependency_links=[
-          'git+https://github.com/fyndiq/confluent_kafka_helpers@v0.5.2#egg=confluent-kafka-helpers-0.5.2'
-      ],
-      zip_safe=False)
+setup(
+    name="eventsourcing-helpers",
+    version="0.6.2",
+    description="Helpers for practicing the Event sourcing pattern",
+    url="https://github.com/fyndiq/eventsourcing_helpers",
+    author="Fyndiq AB",
+    author_email="support@fyndiq.com",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        'structlog>=17.2.0',
+        'cnamedtuple>=0.1.6',
+        'confluent-kafka-helpers==0.5.2'
+    ],
+    dependency_links=[
+        'git+https://github.com/fyndiq/confluent_kafka_helpers@v0.5.2#egg=confluent-kafka-helpers-0.5.2'  # noqa
+    ],
+    zip_safe=False
+)
