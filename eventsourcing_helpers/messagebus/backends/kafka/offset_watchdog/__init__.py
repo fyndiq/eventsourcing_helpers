1ce4b5a9b532a0eac0f56e5ff3beb090eaa40dab
Sergei Beilin (Fyndiq AB) <sergei.beilin@fyndiq.com>
2018-03-01 15:00:15 +0100

    Sketch the OffsetWatchdog base class and In-Memory implementation

---
+++ eventsourcing_helpers/messagebus/backends/kafka/offset_watchdog/__init__.py
@@ -0,0 +1,28 @@
+from eventsourcing_helpers.message import Message
+
+
+class OffsetWatchdog:
+    def __init__(self, consumer_id: str):
+        self._consumer_id = consumer_id
+
+    def _key(self, message: Message):
+        return f'{message._meta.partition}-{message._meta.topic}-{self._consumer_id}'
+
+    def seen(self, message: Message) -> bool:
+        raise NotImplementedError
+
+    def set_seen(self, message: Message):
+        raise NotImplementedError
+
+
+class InMemoryOffsetWatchdog(OffsetWatchdog):
+    def __init__(self, consumer_id: str):
+        super().__init__(consumer_id=consumer_id)
+        self._offset_map = {}
+
+    def seen(self, message: Message) -> bool:
+        last_offset = self._offset_map.get(self._key(message), -1)
+        return message._meta.offset < last_offset
+
+    def set_seen(self, message: Message):
+        self._offset_map[self._key(message)] = message._meta.offset
