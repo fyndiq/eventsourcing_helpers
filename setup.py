9cb584310a2c3baf5a4abfaedac2c430daf2ab82
Sergei Beilin (Fyndiq AB) <sergei.beilin@fyndiq.com>
2018-03-02 17:25:55 +0100

    Add [redis] option by fixing extras_require in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="eventsourcing-helpers",
-      version="0.6.2",
+      version="0.7.0",
       description="Helpers for practicing the Event sourcing pattern",
       url="https://github.com/fyndiq/eventsourcing_helpers",
       author="Fyndiq AB",
@@ -13,6 +13,9 @@ setup(name="eventsourcing-helpers",
           'cnamedtuple>=0.1.6',
           'confluent-kafka-helpers==0.5.2',
       ],
+      extras_require={
+          'redis': ["redis>=2.10.6", "hiredis>=0.2.0"],
+      },
       dependency_links=[
           'git+https://github.com/fyndiq/confluent_kafka_helpers@v0.5.2#egg=confluent-kafka-helpers-0.5.2'
       ],
