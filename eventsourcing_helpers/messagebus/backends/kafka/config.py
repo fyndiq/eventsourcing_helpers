f325cc1b3b62a9d8c27dc30f48a1b6e62a3c05c1
Simon Wahlgren <simon.wahlgren@fyndiq.se>
2018-03-05 10:46:46 +0100

    Restructure files and refactor configuration

--- eventsourcing_helpers/messagebus/backends/kafka/config.py
+++ eventsourcing_helpers/messagebus/backends/kafka/config.py
@@ -1,22 +1,48 @@
+from collections import defaultdict
+from copy import deepcopy
+
+
 def get_producer_config(config):
+    config = deepcopy(config)
     producer_config = config.get('producer', None)
     if not producer_config:
         return None
 
-    producer_config.update({
-        'bootstrap.servers': config['bootstrap.servers'],
-        'schema.registry.url': config['schema.registry.url']
-    })
+    producer_config.update(
+        {
+            'bootstrap.servers': config['bootstrap.servers'],
+            'schema.registry.url': config['schema.registry.url']
+        }
+    )
     return producer_config
 
 
 def get_consumer_config(config):
+    config = deepcopy(config)
     consumer_config = config.get('consumer')
     if not consumer_config:
         return None
 
-    consumer_config.update({
-        'bootstrap.servers': config['bootstrap.servers'],
-        'schema.registry.url': config['schema.registry.url']
-    })
+    consumer_config.pop('offset_watchdog', None)
+    consumer_config.update(
+        {
+            'bootstrap.servers': config['bootstrap.servers'],
+            'schema.registry.url': config['schema.registry.url']
+        }
+    )
     return consumer_config
+
+
+def get_offset_watchdog_config(config):
+    config = deepcopy(config)
+    consumer_config = config.get('consumer', {})
+    if not consumer_config:
+        return None
+
+    offset_wd_config = defaultdict(
+        dict, consumer_config.get('offset_watchdog', {})
+    )
+    offset_wd_config['backend_config'].update(
+        {'group.id': consumer_config['group.id']}
+    )
+    return offset_wd_config
