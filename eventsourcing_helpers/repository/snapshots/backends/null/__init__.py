c5257c5ac3fb37f4d0b585c621b75770b6d910f9
Viktor Melker <viktormelker@gmail.com>
2018-03-20 17:57:13 +0100

    added a null backend for snapshots

---
+++ eventsourcing_helpers/repository/snapshots/backends/null/__init__.py
@@ -0,0 +1,19 @@
+from eventsourcing_helpers.models import AggregateRoot
+from eventsourcing_helpers.repository.snapshots.backends import SnapshotBackend
+
+
+class NullSnapshotBackend(SnapshotBackend):
+    """
+    Provide a dummy snapshot backend that does not do anything
+    """
+
+    def __init__(
+        self, config: dict,
+    ) -> None:
+        pass
+
+    def _save_snapshot(self, aggregate: AggregateRoot) -> None:
+        pass
+
+    def _get_from_snapshot(self, aggregate_id: str) -> AggregateRoot:
+        return None
