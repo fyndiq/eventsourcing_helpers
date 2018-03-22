89724e18909155a705595e2734b0a08771127d2c
Viktor Melker <viktormelker@gmail.com>
2018-03-22 09:46:50 +0100

    added deserializer function

--- eventsourcing_helpers/repository/snapshot_backends/mongo/serializer.py
+++ eventsourcing_helpers/repository/snapshot_backends/mongo/serializer.py
@@ -1,7 +1,14 @@
-def snapshot_serializer(data, version: str, aggregate_hash: str) -> dict:
+from eventsourcing_helpers.models import AggregateRoot
+
+
+def serialize_data(data, version: str, aggregate_hash: int) -> dict:
     snapshot_data = {}
 
     snapshot_data['data'] = data
     snapshot_data['version'] = version
     snapshot_data['hash'] = aggregate_hash
     return snapshot_data
+
+
+def deserialize_data(data: dict) -> (AggregateRoot, str, int):
+    return (data['data'], data['version'], data['hash'])
