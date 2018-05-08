e1a332436dc62351f09cd8c3b26ff74d0949a8de
Sergei Beilin (Fyndiq AB) <sergei.beilin@fyndiq.com>
2018-05-08 11:26:25 +0200

    Update confluent-kafka-helpers==0.6.1; bump version to 0.7.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="eventsourcing-helpers",
-    version="0.7.0",
+    version="0.7.1",
     description="Helpers for practicing the Event sourcing pattern",
     url="https://github.com/fyndiq/eventsourcing_helpers",
     author="Fyndiq AB",
@@ -12,13 +12,13 @@ setup(
     install_requires=[
         'structlog>=17.2.0',
         'cnamedtuple>=0.1.6',
-        'confluent-kafka-helpers==0.5.2'
+        'confluent-kafka-helpers==0.6.1'
     ],
     extras_require={
         'redis': ["redis>=2.10.6", "hiredis>=0.2.0"],
     },
     dependency_links=[
-        'git+https://github.com/fyndiq/confluent_kafka_helpers@v0.5.2#egg=confluent-kafka-helpers-0.5.2'  # noqa
+        'git+https://github.com/fyndiq/confluent_kafka_helpers@v0.6.1#egg=confluent-kafka-helpers'  # noqa
     ],
     zip_safe=False
 )
