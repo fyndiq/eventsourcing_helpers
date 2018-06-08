91bdcac33dbf9c3d20a5dc71881055b8d47c25d6
Viktor Melker <viktormelker@gmail.com>
2018-06-08 13:10:22 +0200

    Add comments

--- eventsourcing_helpers/utils.py
+++ eventsourcing_helpers/utils.py
@@ -42,19 +42,20 @@ def get_all_nested_keys(data: dict, current_keys: List) -> List:
         List: All keys found
 
     Example:
-    >>> get_all_nested_keys({
-            '_version': 0,
-            'id': None,
-            'nested_entity': {
-                '__dict__': {
-                    'nested_entity_id': 'a'
-                },
-                'py/object': 'eventsourcing_helpers.models.EntityDict'
-            },
-            'py/object': 'tests.test_models.NestedAggregate'
-        },
-        []
-    )
+    >>> get_all_nested_keys(
+    ...     {
+    ...         '_version': 0,
+    ...         'id': None,
+    ...         'nested_entity': {
+    ...             '__dict__': {
+    ...                 'nested_entity_id': 'a'
+    ...             },
+    ...             'py/object': 'eventsourcing_helpers.models.EntityDict'
+    ...         },
+    ...         'py/object': 'tests.test_models.NestedAggregate'
+    ...     },
+    ...     []
+    ... )
 
     [
         '_version', 'id', 'nested_entity', 'py/object', '__dict__',
