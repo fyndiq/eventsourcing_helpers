5fbfe81a6ceb2b3c441d8734546df13cd3f8a636
Simon Wahlgren <simon.wahlgren@fyndiq.se>
2018-10-02 15:19:15 +0200

    Support max_offset and ignore_missing_apply_methods arguments

--- eventsourcing_helpers/repository/__init__.py
+++ eventsourcing_helpers/repository/__init__.py
@@ -33,17 +33,16 @@ class Repository:
 
     def __init__(
         self, config: dict, aggregate_root_cls: AggregateRoot,
-        importer: Callable = import_backend,
-        message_deserializer: Callable = from_message_to_dto,
-        snapshot=Snapshot, **kwargs
+        ignore_missing_apply_methods: bool = False, importer: Callable = import_backend,
+        message_deserializer: Callable = from_message_to_dto, snapshot=Snapshot, **kwargs
     ) -> None:
         backend_path = config.get('backend', BACKENDS[self.DEFAULT_BACKEND])
         assert 'backend_config' in config, "You must pass a backend config"
         backend_config = config.get('backend_config')
 
         logger.info(
-            "Using repository backend", backend=backend_path,
-            config=backend_config
+            "Using repository backend", backend=backend_path, config=backend_config,
+            ignore_missing_apply_methods=ignore_missing_apply_methods
         )
         backend_class = importer(backend_path)
 
@@ -51,6 +50,8 @@ class Repository:
         self.message_deserializer = message_deserializer
         self.snapshot = snapshot(config, **kwargs)
         self.backend = backend_class(backend_config, **kwargs)
+
+        self.ignore_missing_apply_methods = ignore_missing_apply_methods
 
     def commit(self, aggregate_root: AggregateRoot, **kwargs) -> None:
         """
@@ -70,24 +71,21 @@ class Repository:
                 self.backend.commit(id=id, events=events, **kwargs)
             except KafkaException as e:
                 logger.info("Kafka commit failed, rolling back snapshot!")
-                statsd.increment(
-                    'eventsourcing_helpers.snapshot.cache.delete',
-                    tags=[f'id={id}']
-                )
+                statsd.increment('eventsourcing_helpers.snapshot.cache.delete', tags=[f'id={id}'])
                 self.snapshot.delete(aggregate_root)
                 raise e
 
             aggregate_root._clear_staged_events()
 
-    def load(self, id: str) -> AggregateRoot:
+    def load(self, id: str, max_offset: int = None) -> AggregateRoot:
         """
         Load an aggregate root accordingly:
-
-        1. First try to load it from the snapshot storage.
-        2. If there are no snapshot load it from the event storage.
+         - First try to load it from the snapshot storage.
+         - If there are no snapshot load it from the event storage.
 
         Args:
             id: ID of the aggregate root.
+            max_offset: Stop loading events at this position.
 
         Returns:
             AggregateRoot: Aggregate root instance with the latest state.
@@ -95,7 +93,7 @@ class Repository:
         aggregate_root = self._load_from_snapshot_storage(id)
         if aggregate_root is None:
             statsd.increment('eventsourcing_helpers.snapshot.cache.misses')
-            aggregate_root = self._load_from_event_storage(id)
+            aggregate_root = self._load_from_event_storage(id, max_offset)
             logger.debug("Aggregate was loaded from event storage")
         else:
             statsd.increment('eventsourcing_helpers.snapshot.cache.hits')
@@ -117,18 +115,22 @@ class Repository:
 
         return aggregate_root
 
-    def _load_from_event_storage(self, id: str) -> AggregateRoot:
+    def _load_from_event_storage(self, id: str, max_offset: int) -> AggregateRoot:
         """
         Load the aggregate from the event storage.
 
         Args:
             id: ID of the aggregate root.
+            max_offset: Stop loading events at this position.
 
         Returns:
             AggregateRoot: Aggregate root instance with the latest state.
         """
         aggregate_root = self.aggregate_root_cls()
-        events = self.backend.get_events(id, self.message_deserializer)
-        aggregate_root._apply_events(events)
-
+        events = self.backend.get_events(
+            id, message_deserializer=self.message_deserializer, max_offset=max_offset
+        )
+        aggregate_root._apply_events(
+            events, ignore_missing_apply_methods=self.ignore_missing_apply_methods
+        )
         return aggregate_root
