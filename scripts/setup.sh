5446f06c18388b58e9cb0bf9389ff0e06e7a8184
Simon Wahlgren <simon.wahlgren@fyndiq.se>
2018-11-01 22:16:41 +0100

    Install requirements correctly

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,4 @@
-#!/usr/bin/env sh
-command -v virtualenv >/dev/null 2>&1 || echo "virtualenv is required"
-
+#!/bin/bash
 virtualenv -p python3 .venv
 source .venv/bin/activate
-pip install --process-dependency-links --no-cache-dir -r requirements.txt
+pip install --process-dependency-links --no-cache-dir -r requirements/dev.txt
