de1c80ffb32413602b1430ee12f91736ce32ccab
Simon Wahlgren <simon.wahlgren@fyndiq.se>
2018-12-03 10:58:53 +0100

    Remove comments

--- eventsourcing_helpers/messagebus/backends/kafka/__init__.py
+++ eventsourcing_helpers/messagebus/backends/kafka/__init__.py
@@ -87,10 +87,6 @@ class KafkaAvroBackend(MessageBusBackend):
 
         self.producer.produce(key=key, value=value, topic=topic, **kwargs)
         self.producer.poll(0)
-        # if 'callback' in kwargs:
-        #     # The poll will ensure that the callback for the _previous_
-        #     # produce call gets called
-        #     self.producer.poll(0)
         if self.flush:
             self.producer.flush()
 
