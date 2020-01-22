f3c6350f1dbfa02259d622eabb2eac8c5e997e5b
Simon Wahlgren <simon.wahlgren@gmail.com>
2020-01-22 09:24:48 +0100

    refactor setup.sh using builtin venv module

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,5 @@
-#!/usr/bin/env sh
-command -v virtualenv >/dev/null 2>&1 || echo "virtualenv is required"
-
-virtualenv -p python3 .venv
+#!/usr/bin/env bash
+set -e
+python3 -m venv .venv
 source .venv/bin/activate
-pip install --process-dependency-links --no-cache-dir -r requirements.txt
+pip install -r requirements.txt
