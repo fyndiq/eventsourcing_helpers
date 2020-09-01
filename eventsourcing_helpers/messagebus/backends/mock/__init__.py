8fb89a84733c7d4a686ad7e9d44197609fbc5181
Simon Wahlgren <simon.wahlgren@gmail.com>
2020-09-01 12:20:16 +0200

    messagebus.backends.mock: merge kwargs in assert_one_message_produced_with

--- eventsourcing_helpers/messagebus/backends/mock/__init__.py
+++ eventsourcing_helpers/messagebus/backends/mock/__init__.py
@@ -43,11 +43,9 @@ class Producer:
     def clear_messages(self) -> None:
         self.messages.clear()
 
-    def assert_one_message_produced_with(self, key: str, value: dict, headers: dict = None) -> None:
-        if headers is None:
-            headers = {}
+    def assert_one_message_produced_with(self, key: str, value: dict, **kwargs) -> None:
         assert len(self.messages) == 1
-        assert dict(key=key, value=value, headers=headers) in self.messages
+        assert dict(key=key, value=value, **kwargs) in self.messages
 
     def assert_no_messages_produced(self) -> None:
         assert len(self.messages) == 0
