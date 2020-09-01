a89cdec1f76b96f74f0c16a1f8b21d75094d0df1
Simon Wahlgren <simon.wahlgren@gmail.com>
2020-09-01 11:33:15 +0200

    setup.py: bump version to 0.8.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="eventsourcing-helpers",
-    version="0.8.6",
+    version="0.8.7",
     description="Helpers for practicing the Event sourcing pattern",
     url="https://github.com/fyndiq/eventsourcing_helpers",
     author="Fyndiq AB",
@@ -13,7 +13,7 @@ setup(
     install_requires=[
         'structlog>=17.2.0',
         'jsonpickle==0.9.6',
-        'confluent-kafka-helpers==0.7.4'
+        'confluent-kafka-helpers==0.7.5'
     ],
     extras_require={
         'mongo': ["pymongo==3.6.1"],
